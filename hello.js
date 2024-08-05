e58ecf67cf5b713be63992fdb40a6702daf4b3d5
wcadena <wcadena@outlook.com>
2024-08-04 20:06:00 -0500

    actualiza mensaje

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hola mundo desde Quito, Ecuador!!";
+const hello = () => "Hola mundo desde Quito, Ecuador, vamos DEVOPS!!";
 console.log(hello());
 exports.hello = hello;
 
--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hola mundo desde Quito, Ecuador!!");
+    expect(hello.hello()).toEqual("Hola mundo desde Quito, Ecuador, vamos DEVOPS!!");
   });
 });
