99d68f7f1f892278e381894f09f886a4fcf41674
wcadena <wcadena@outlook.com>
2024-08-04 21:24:59 -0500

    se actauliza el sistema para ver desde cero

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hola mundo desde Quito, Ecuador, vamos DEVOPS!!";
+const hello = () => "Hola mundo desde Quito, Ecuador, vamos DEVOPS, Prueba 1!!";
 console.log(hello());
 exports.hello = hello;
 
